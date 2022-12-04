7c349cc81fc8a5024d8d1626bb4905fa40e6cbfb
Panagiotis Iatrou <iatrou.panag@gmail.com>
2022-12-04 14:59:21 +0200

    Improved GUI scaling

--- src/gui.py
+++ src/gui.py
@@ -37,7 +37,7 @@ class GUI:
 
         # Set the window properties
         master.title("Chess")
-        master.geometry("370x440")
+        master.geometry("380x490")
         master.iconphoto(True, tk.PhotoImage(file="src/assets/pawn_32x32.png"))
         master.resizable(False, False)
         master.attributes('-topmost', True)
@@ -47,92 +47,119 @@ class GUI:
         style = ttk.Style()
         style.theme_use('clam')
 
-        # Separator
-        ttk.Separator(master, orient='horizontal').place(x=0, y=0, width=350)
+        # Left frame
+        left_frame = tk.Frame(master)
 
         # Create the status text
-        tk.Label(master, text="Status:").place(x=0, y=5)
-        self.status_text = tk.Label(master, text="Inactive", fg="red")
-        self.status_text.place(x=45, y=5)
+        status_label = tk.Frame(left_frame)
+        tk.Label(status_label, text="Status:").pack(side=tk.LEFT)
+        self.status_text = tk.Label(status_label, text="Inactive", fg="red")
+        self.status_text.pack()
+        status_label.pack(anchor=tk.NW)
 
         # Create the website chooser radio buttons
         self.website = tk.StringVar(value="chesscom")
-        self.chesscom_radio_button = tk.Radiobutton(master, text="Chess.com", variable=self.website, value="chesscom")
-        self.chesscom_radio_button.place(x=0, y=25)
-        self.lichess_radio_button = tk.Radiobutton(master, text="Lichess.org", variable=self.website, value="lichess")
-        self.lichess_radio_button.place(x=0, y=45)
+        self.chesscom_radio_button = tk.Radiobutton(left_frame, text="Chess.com", variable=self.website, value="chesscom")
+        self.chesscom_radio_button.pack(anchor=tk.NW)
+        self.lichess_radio_button = tk.Radiobutton(left_frame, text="Lichess.org", variable=self.website, value="lichess")
+        self.lichess_radio_button.pack(anchor=tk.NW)
 
         # Create the open browser button
         self.opening_browser = False
         self.opened_browser = False
-        self.open_browser_button = tk.Button(master, text="Open Browser", command=self.on_open_browser_button_listener)
-        self.open_browser_button.place(x=5, y=70)
+        self.open_browser_button = tk.Button(left_frame, text="Open Browser", command=self.on_open_browser_button_listener)
+        self.open_browser_button.pack(anchor=tk.NW)
 
         # Create the start button
         self.running = False
-        self.start_button = tk.Button(master, text="Start", command=self.on_start_button_listener)
+        self.start_button = tk.Button(left_frame, text="Start", command=self.on_start_button_listener)
         self.start_button["state"] = "disabled"
-        self.start_button.place(x=5, y=100)
+        self.start_button.pack(anchor=tk.NW, pady=5)
 
         # Create the bongcloud check button
         self.enable_bongcloud = tk.IntVar()
-        self.bongcloud_check_button = tk.Checkbutton(window, text='Bongcloud', variable=self.enable_bongcloud, onvalue=1, offvalue=0)
-        self.bongcloud_check_button.place(x=0, y=125)
+        self.bongcloud_check_button = tk.Checkbutton(left_frame, text='Bongcloud', variable=self.enable_bongcloud, onvalue=1, offvalue=0)
+        self.bongcloud_check_button.pack(anchor=tk.NW)
 
         # Separator
-        ttk.Separator(master, orient='horizontal').place(x=0, y=165, width=195)
-        tk.Label(master, text="Stockfish parameters").place(x=35, y=155)
+        separator_frame = tk.Frame(left_frame)
+        separator = ttk.Separator(separator_frame, orient='horizontal')
+        separator.grid(row=0, column=0, sticky="ew")
+        label = tk.Label(separator_frame, text="Stockfish parameters")
+        label.grid(row=0, column=0, padx=40)
+        separator_frame.pack(anchor=tk.NW, pady=10, expand=True, fill=tk.X)
 
         # Create the Slow mover entry field
-        tk.Label(master, text="Slow Mover").place(x=0, y=185)
+        slow_mover_frame = tk.Frame(left_frame)
+        self.slow_mover_label = tk.Label(slow_mover_frame, text="Slow Mover")
+        self.slow_mover_label.pack(side=tk.LEFT)
         self.slow_mover = tk.IntVar(value=100)
-        self.slow_mover_entry = tk.Entry(master, textvariable=self.slow_mover, width=35)
-        self.slow_mover_entry.place(x=70, y=185, width=40)
+        self.slow_mover_entry = tk.Entry(slow_mover_frame, textvariable=self.slow_mover, justify="center", width=8)
+        self.slow_mover_entry.pack()
+        slow_mover_frame.pack(anchor=tk.NW)
 
         # Create the skill level scale
-        tk.Label(master, text="Skill Level").place(x=0, y=220)
+        skill_level_frame = tk.Frame(left_frame)
+        tk.Label(skill_level_frame, text="Skill Level").pack(side=tk.LEFT, pady=19)
         self.skill_level = tk.IntVar(value=20)
-        self.skill_level_scale = tk.Scale(master, from_=0, to=20, orient=tk.HORIZONTAL, variable=self.skill_level)
-        self.skill_level_scale.place(x=60, y=203, width=100)
+        self.skill_level_scale = tk.Scale(skill_level_frame, from_=0, to=20, orient=tk.HORIZONTAL, variable=self.skill_level)
+        self.skill_level_scale.pack()
+        skill_level_frame.pack(anchor=tk.NW)
 
         # Create the memory entry field
-        tk.Label(master, text="Memory").place(x=0, y=255)
+        memory_frame = tk.Frame(left_frame)
+        tk.Label(memory_frame, text="Memory").pack(side=tk.LEFT)
         self.memory = tk.IntVar(value=512)
-        self.memory_entry = tk.Entry(master, textvariable=self.memory, justify="center", width=35)
-        self.memory_entry.place(x=52, y=255, width=40)
-        tk.Label(master, text="MB").place(x=92, y=255)
+        self.memory_entry = tk.Entry(memory_frame, textvariable=self.memory, justify="center", width=9)
+        self.memory_entry.pack(side=tk.LEFT)
+        tk.Label(memory_frame, text="MB").pack()
+        memory_frame.pack(anchor=tk.NW, pady=(0,15))
 
         # Create the CPU threads entry field
-        tk.Label(master, text="CPU Threads").place(x=0, y=285)
+        cpu_threads_frame = tk.Frame(left_frame)
+        tk.Label(cpu_threads_frame, text="CPU Threads").pack(side=tk.LEFT)
         self.cpu_threads = tk.IntVar(value=1)
-        self.cpu_threads_entry = tk.Entry(master, textvariable=self.cpu_threads, justify="center", width=35)
-        self.cpu_threads_entry.place(x=75, y=285, width=30)
+        self.cpu_threads_entry = tk.Entry(cpu_threads_frame, textvariable=self.cpu_threads, justify="center", width=7)
+        self.cpu_threads_entry.pack()
+        cpu_threads_frame.pack(anchor=tk.NW)
 
         # Separator
-        ttk.Separator(master, orient='horizontal').place(x=0, y=325, width=195)
-        tk.Label(master, text="Misc").place(x=75, y=315)
+        separator_frame = tk.Frame(left_frame)
+        separator = ttk.Separator(separator_frame, orient='horizontal')
+        separator.grid(row=0, column=0, sticky="ew")
+        label = tk.Label(separator_frame, text="Misc")
+        label.grid(row=0, column=0, padx=82)
+        separator_frame.pack(anchor=tk.NW, pady=10, expand=True, fill=tk.X)
 
         # Create the topmost check button
         self.enable_topmost = tk.IntVar(value=1)
-        self.topmost_check_button = tk.Checkbutton(window, text='Window stays on top', variable=self.enable_topmost, onvalue=1, offvalue=0, command=self.on_topmost_check_button_listener)
-        self.topmost_check_button.place(x=0, y=335)
+        self.topmost_check_button = tk.Checkbutton(left_frame, text='Window stays on top', variable=self.enable_topmost, onvalue=1, offvalue=0, command=self.on_topmost_check_button_listener)
+        self.topmost_check_button.pack(anchor=tk.NW)
 
         # Create the select stockfish button
         self.stockfish_path = ""
-        self.select_stockfish_button = tk.Button(master, text="Select Stockfish", command=self.on_select_stockfish_button_listener)
-        self.select_stockfish_button.place(x=5, y=360)
+        self.select_stockfish_button = tk.Button(left_frame, text="Select Stockfish", command=self.on_select_stockfish_button_listener)
+        self.select_stockfish_button.pack(anchor=tk.NW)
 
         # Create the stockfish path text
-        self.stockfish_path_text = tk.Label(master, text="", wraplength=180)
-        self.stockfish_path_text.place(x=5, y=385)
+        self.stockfish_path_text = tk.Label(left_frame, text="", wraplength=180)
+        self.stockfish_path_text.pack(anchor=tk.NW)
+
+        left_frame.grid(row=0, column=0, padx=5, sticky=tk.NW)
+
+        # Right frame
+        right_frame = tk.Frame(master)
+
+        # Treeview frame
+        treeview_frame = tk.Frame(right_frame)
 
         # Create the moves Treeview
-        self.tree = ttk.Treeview(master, column=("#", "White", "Black"), show='headings', height=19, selectmode='browse')
-        self.tree.place(x=195, y=0)
+        self.tree = ttk.Treeview(treeview_frame, column=("#", "White", "Black"), show='headings', height=21, selectmode='browse')
+        self.tree.pack(anchor=tk.NW, side=tk.LEFT)
 
-        # Add the scrollbar to the Treeview
-        self.vsb = ttk.Scrollbar(master, orient="vertical", command=self.tree.yview)
-        self.vsb.place(x=195 + 155 + 5, y=0, height=190 * 2 + 29)
+        # # Add the scrollbar to the Treeview
+        self.vsb = ttk.Scrollbar(treeview_frame, orient="vertical", command=self.tree.yview)
+        self.vsb.pack(fill=tk.Y, expand=True)
         self.tree.configure(yscrollcommand=self.vsb.set)
 
         # Create the columns
@@ -143,9 +170,13 @@ class GUI:
         self.tree.column("# 3", anchor=tk.CENTER, width=60)
         self.tree.heading("# 3", text="Black")
 
+        treeview_frame.pack(anchor=tk.NW)
+
         # Create the export PGN button
-        self.export_pgn_button = tk.Button(master, text="Export PGN", command=self.on_export_pgn_button_listener)
-        self.export_pgn_button.place(x=195, y=410, width=174)
+        self.export_pgn_button = tk.Button(right_frame, text="Export PGN", command=self.on_export_pgn_button_listener)
+        self.export_pgn_button.pack(anchor=tk.NW, fill=tk.X)
+
+        right_frame.grid(row=0, column=1, sticky=tk.NW)
 
         # Start the process checker thread
         process_checker_thread = threading.Thread(target=self.process_checker_thread)
