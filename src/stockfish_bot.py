70dd0e8bd4e1cf32002e64038f45e8a0cdc532ca
Mark <14284867+xHeaven@users.noreply.github.com>
2023-01-09 22:59:50 +0100

    websocket move only for lichess

--- src/stockfish_bot.py
+++ src/stockfish_bot.py
@@ -208,7 +208,10 @@ class StockfishBot(multiprocess.Process):
                         stockfish.make_moves_from_current_position([move])
                         move_list.append(move_san)
                         self.grabber.ws_execute_move(move, move_count + 1)
-                        # self.make_move(move)
+                        if self.website == "lichess":
+                            self.grabber.ws_execute_move(move, move_count + 1)
+                        else:
+                            self.make_move(move)
 
                     self.overlay_queue.put([])
 
